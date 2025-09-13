ee5a4fccfde8272cd8e7cbf1d1c28bc2356d5e68
pavanpappala32 <pavanpappala32@gmail.com>
2025-09-13 12:02:13 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const port=process.env.PORT || 5000
 
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://chatly-st7q.onrender.com",
     credentials:true
 }))
 app.use(express.json())
